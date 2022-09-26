f75bd7e1a829001f04bc24d59eefd95d6e5d0266
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-09-26 10:36:58 -0700

    unction that replaces an element of a list at a specific position (like in C).

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    """
-    function that retrieves an element from a given index of
-    a list like in C.
-    """
     if my_list:
         if (idx < 0) or (idx > len(my_list)):
             return None
         else:
-            return my_list[idx]
+            return (my_list[idx])
