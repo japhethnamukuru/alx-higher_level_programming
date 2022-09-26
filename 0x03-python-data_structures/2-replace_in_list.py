78ecbb031709672ecf70541f950bf31589e64a35
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-09-26 10:33:40 -0700

    unction that replaces an element of a list at a specific position (like in C).

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    """
-    unction that replaces an element of a list at a specific position (like in C).
-
-    """
     if (idx < 0) or (idx > len(my_list)):
         return my_list
     else:
