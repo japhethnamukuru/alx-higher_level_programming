93f684ccdb16e8633cc81d734fa96868e4adb859
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-09-29 06:28:32 -0700

    Added readme, unction that computes the square value of all integers of a matrix.

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    sq_matrix = []
+    for x in matrix:
+        sq_matrix.append(list(map(lambda x: x ** 2, x)))
+    return sq_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,3 @@
+## 0x04. Python - More Data Structures: Set, Dictionary
+
+Introdeces the fundamenta concepts of sets and dictionary data structure
