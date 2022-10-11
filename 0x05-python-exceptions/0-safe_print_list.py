a13e54a37d776ebfd2ff8cc1edbc5573bc849981
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-11 09:31:08 -0700

    modified 0-safe_print.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        i = 0
-        for item in my_list[:x]:
-            print("{}".format(item), end='')
-            i += 1
-        print("\n")
-    except TypeError:
-        print("could not print items")
-    else:
-        return (i)
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except IndexError:
+            break
+        else:
+            count += 1
+    print()
+
+    return count
