149dd5bae7a62dba5b351f64dfc173d845dac4be
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 07:09:19 -0700

    modified files

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,4 +6,6 @@ class Square():
     """class Square that defines a square by: (based on 0-square.py)"""
     
     def __init__(self, size):
+        """Initialize the class"""
+
         self.__size = size
