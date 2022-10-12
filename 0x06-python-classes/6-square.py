f3d04c4fdaf395ac5209fb4304cab4be150af900
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 08:02:41 -0700

    Private instance attribute: position:

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -27,10 +27,16 @@ class Square():
 
     @position.setter
     def position(self, value):
-        for i in value:
-            if (type(i) is not int) or (i < 0):
-                raise TypeError("position must be a tuple of 2 positive integers")
-        return self.__position
+        if type(value) is not tuple:
+            raise TypeError("position must must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (value[0] is not int) or (value[1] is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (value[0] < 0) or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
             
 
     def area(self):
