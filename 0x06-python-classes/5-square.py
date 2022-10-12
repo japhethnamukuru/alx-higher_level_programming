49dd656d94cae6001e044e9c28aeb91e4335bca0
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 07:25:18 -0700

    Public instance method: def my_print(self): that prints in stdout the square with the character #:

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Working with classes, python OOP"""
+
+
+class Square():
+    """Class that tries to model a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """property for retrieving size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Instance method that computes area of a square
+            Args:
+                self
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        my_value = self.__size
+        if my_value == 0:
+            print()
+        else:
+            for i in range(my_value):
+                for j in range(my_value):
+                    print("#", end='')
+                print()
