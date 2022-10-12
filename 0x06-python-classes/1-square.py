d29f172f2cb8aebd79d3ddfa22c4f87c18d86eba
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 05:54:19 -0700

    modified 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """class Square that defines a square by: (based on 0-square.py)"""
 
+
 class Square():
     """class Square that defines a square by: (based on 0-square.py)"""
 
