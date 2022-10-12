795df607e5563a224f53f0204569406d7d57de3d
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 06:31:16 -0700

    class Square that defines a square by: (based on 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,7 +14,6 @@ class Square():
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
     def area(self):
         """Instance method that computes area of a square"""
         return (self.__size * self.__size)
