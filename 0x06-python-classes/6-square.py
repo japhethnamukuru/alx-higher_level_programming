ec43838795b0111374dcb72697e70db83c0df437
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 08:12:12 -0700

    Private instance attribute: position:

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,6 +15,7 @@ class Square():
 
     @size.setter
     def size(self, value):
+        """set size"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
