1b27d56f22073ecd51cd682e7ca96db1e9c0e2e9
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 07:02:52 -0700

    modified files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""empty class Square that defines a square"""
+"""class Square that defines a square"""
+
 
 class Square():
     """empty class"""
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,9 +4,6 @@
 
 class Square():
     """class Square that defines a square by: (based on 0-square.py)"""
+    
     def __init__(self, size):
-        """ initialize the class
-            Args:
-                size (int): size of the square
-        """
         self.__size = size
