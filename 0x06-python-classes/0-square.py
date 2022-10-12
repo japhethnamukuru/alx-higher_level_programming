ca405bc579f6001bde59136ca0e21a244d3868c2
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 05:31:18 -0700

    modified 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-'''empty class Square that defines a square:'''
+"""empty class Square that defines a square"""
+
+
 
 class Square():
-    '''empty class'''
+    """empty class"""
     pass
