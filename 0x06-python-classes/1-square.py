def8451157225c8f2080075a70bea19ab9a671a2
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-12 05:52:29 -0700

    modified 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,4 +5,8 @@ class Square():
     """class Square that defines a square by: (based on 0-square.py)"""
 
     def __init__(self, size):
+        """ initialize the class
+            Args:
+                size (int): size of the square
+        """
         self.__size = size
