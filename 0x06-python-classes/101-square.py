5aca7bc44fc1a0e81b7c2717a9dfbd77696b324a
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-13 14:19:23 -0700

    update files

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -59,3 +59,14 @@ class Square():
             for i in range(self.size):
                 print(" " * self.position[0], end="")
                 print("#" * self.size)
+    
+    def __str__(self):
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
+
