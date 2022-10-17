f60450b11722436d3fecfb30eeb3d039e63e20c7
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-17 11:48:06 -0700

    class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+"""class thet represents a rectangle"""
+
+
+class Rectangle:
+    """Rectangle class """
+
+    def __init__(self, width=0, height=0):
+        """the ractangle constructor"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """get area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """get perimeter"""
+        if (self.width == 0) or (self.height == 0):
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """string representation"""
+        if self.width == 0 or self.height == 0:
+            print("")
+
+        rect = []
+        for i in range(self.height):
+            [rect.append("#") for j in range(self.width)]
+            if i != self.height - 1:
+                rect.append("\n")
+        return "".join(rect)
+
