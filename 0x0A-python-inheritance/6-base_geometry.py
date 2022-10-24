dd5ed3cbbf12feb1046883a6c7f28cf0a1aa17dd
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-24 03:06:24 -0700

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python OOP, empty class that attempts to model a geometry"""
+
+
+class BaseGeometry:
+    """Empty class for modelling geometry"""
+
+    def area(self):
+        """Raise Exception"""
+
+        raise Exception("area() is not implemented")
