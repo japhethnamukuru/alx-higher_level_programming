56ab9b3a1efc6f0b40078995482ece0dfc2ec45b
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-24 01:57:10 -0700

    class MyList that inherits from pythons builtin list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""python OOP, class that inherits from list"""
+
+
+class MyList(list):
+    """custom class that inheits from pythons list class"""
+
+    def print_sorted(self):
+        """sort the list in ascending order"""
+
+        print(sorted(self))
