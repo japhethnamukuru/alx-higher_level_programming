ab21d1e905d876c63974348670c92f20ea6d2a3e
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-24 02:14:25 -0700

    checking for instances

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""functon for checkimg if a given object is an instance of the given class"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an instance of the specified class ; otherwise False"""
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
