743acfc0043afa048432b4a145e01cd321dd475a
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-24 04:15:52 -0700

    rectange class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python OOP, subclassing a class through inheritance"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
