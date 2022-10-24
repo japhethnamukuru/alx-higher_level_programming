370516c2b0e617fd18e5d162ff477fd47291602c
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-24 05:16:28 -0700

    implement area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python OOP, subclassing a class through inheritance"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Compute the area"""
+
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """object representation"""
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
