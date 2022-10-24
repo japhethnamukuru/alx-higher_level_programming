4e6ddc86fd5ed167d9511e13cf079fdb20702aea
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-24 05:25:21 -0700

    sqaure class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python OOP, subclassing a class through inheritance"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Rectangle inherits from BaseGeometry"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Compute the area"""
+
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """object representation"""
+
+        return f"[Rectangle] {self.__size}/{self.__size}"
