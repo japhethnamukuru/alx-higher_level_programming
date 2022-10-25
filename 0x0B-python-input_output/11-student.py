caa65fd0586fe08491721375eb8b2c63cbafc48c
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-25 13:28:17 -0700

    setting attributes json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""""Python file IO, returning a class variables"""
+
+
+class Student:
+    """Student class that attempts to model a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrive instance dict"""
+
+        if type(attrs) is list and all(type(element) is str for element in attrs):
+            return {k : getattr(self, k) for k in attrs if hasattr(self, k)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all the attributes of an instance"""
+
+        for k, v in json.items():
+            setattr(self, k, v)
