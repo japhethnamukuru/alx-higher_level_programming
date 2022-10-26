0473d54bd3ecdc5b810846431ea085f0089b8655
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-26 12:18:59 -0700

    update files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """Read a file and print its contents"""
     with open(filename, 'r', encoding="utf-8") as file_obj:
         text = file_obj.read()
-        print(text)
+        print(text, end="")
