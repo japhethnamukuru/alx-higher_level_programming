d177fdfb3dc82cfe6649d274401ff1303bacb91c
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-30 14:15:30 -0700

    square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Python OOP"""
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
