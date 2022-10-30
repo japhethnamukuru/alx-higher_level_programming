43feed052cc01448d58e4dc1a1b07ff4cbdcb68c
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-30 13:21:27 -0700

    rectangle update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -62,13 +62,53 @@ class TestRectangleMethods(unittest.TestCase):
     def setUp(self):
         self.rect = Rectangle(3, 4)
         self.rect1 = Rectangle(4, 6, 2, 1, 12)
-        self.rect1.update(89)
 
     def test_rectangle_area_method(self):
         self.assertEqual(self.rect.area(), 12)
 
-    def test_rectangle_update_method(self):
+    def test_rectangle_update_method_args_id(self):
+        self.rect1.update(89)
         self.assertEqual(self.rect1.id, 89)
+
+    def test_rectangle_update_method_args_width(self):
+        self.rect1.update(89, 2)
+        self.assertEqual(self.rect1.width, 2)
+
+    def test_rectangle_update_method_args_height(self):
+        self.rect1.update(89, 2, 3)
+        self.assertEqual(self.rect1.height, 3)
+
+    def test_rectangle_update_method_args_x(self):
+        self.rect1.update(89, 2, 3, 4)
+        self.assertEqual(self.rect1.x, 4)
+
+    def test_rectangle_update_method_args_y(self):
+        self.rect1.update(89, 2, 3, 4, 5)
+        self.assertEqual(self.rect1.y, 5)
+
+
+    def test_update_method_kwargs_height(self):
+        self.rect1.update(height=1)
+        self.assertEqual(self.rect1.height, 1)
+
+    def test_rectangle_method_kwargs_width(self):
+        self.rect1.update(width=1, x=2)
+        self.assertEqual(self.rect1.width, 1)
+        self.assertEqual(self.rect1.x, 2)
+
+    def test_rectangle_method_kwargs_y(self):
+        self.rect1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(self.rect1.y, 1)
+        self.assertEqual(self.rect1.width, 2)
+        self.assertEqual(self.rect1.x, 3)
+        self.assertEqual(self.rect1.id, 89)
+
+    def test_update_method_kwargs_x(self):
+        self.rect1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(self.rect1.x,1)
+        self.assertEqual(self.rect1.height, 2)
+        self.assertEqual(self.rect1.y, 3)
+        self.assertEqual(self.rect1.width, 4)
 
 
 if __name__ == '__main__':
