e7962d664e1ef4e5f824a46663bdc589b4874508
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-31 04:44:43 -0700

    update base class to_json method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """Python OOP, unit tests and file IO"""
+import json
 
 
 class Base:
@@ -14,3 +15,10 @@ class Base:
            self.id = Base.__nb_of_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
