09da5437f58f53df8d7cd89dbc70545feee79d9d
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-31 03:07:06 -0700

    square tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -15,3 +15,22 @@ class TestSquareattributes(unittest.TestCase):
 
     def test_square_area_method(self):
         self.assertEqual(self.sq.area(), 25)
+
+    def test_update_method_args_id(self):
+        self.asserEqual(self.sq.id, 1)
+
+    def test_update_method_args_id(self):
+        self.sq.update(10)
+        self.assertEqual(self.sq.id, 10)
+
+    def test_update_method_args_size(self):
+        self.sq.update(1, 2)
+        self.assertEqual(self.sq.size, 2)
+
+    def test_update_method_args_x(self):
+        self.sq.update(1, 2, 3)
+        self.assertEqual(self.sq.x, 3)
+
+    def test_update_method_args_y(self):
+        self.sq.update(1, 2, 3, 4)
+        self.assertEqual(self.sq.y, 4)
