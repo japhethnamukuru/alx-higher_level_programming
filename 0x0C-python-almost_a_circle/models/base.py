6bc78d3ae485839c4eb1f61618770752700a9fe5
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-31 12:06:18 -0700

    update base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,10 @@ class Base:
 
         with open(json_file, 'w') as file_obj:
             file_obj.write(obj_list_str)
+    
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
