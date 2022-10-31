a0ff7f14bddd26e803ac30f737e5a60052657050
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-31 04:58:10 -0700

    test base to json method

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,16 +2,23 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 class TestBaseModel(unittest.TestCase):
     def setUp(self):
-        self.my_obj = Base()
+        self.my_obj = Rectangle(10, 7, 2, 8)
 
     def test_instance_id(self):
         self.assertIsNot(self.my_obj.id, None)
 
     def test_class_instance(self):
         self.assertIsInstance(self.my_obj, Base)
+
+    def test_to_json_method(self):
+        dictionary = self.my_obj.to_dictionary()
+        json_str = Base.to_json_string([dictionary])
+        self.assertIsInstance(dictionary, dict)
+        self.assertIsInstance(json_str, str)
 
 if __name__ == "__main__":
     unittest.main()
